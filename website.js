6ea23a39a87b0668cc59eeb1378e8d2e08d61821
matthewleemle <matthewleemle@gmail.com>
2019-08-09 22:03:46 +0800

    Personal website

---
+++ website.js
@@ -0,0 +1,20 @@
+
+$("#projects").click(function() {
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $("#robo").offset().top
+    }, 1000);
+});
+
+
+$("#selfintro").click(function() {
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $("#introstart").offset().top
+    }, 2000);
+});
+
+
+$("#homebut").click(function() {
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $("#homefoto").offset().top
+    }, 2000);
+});
